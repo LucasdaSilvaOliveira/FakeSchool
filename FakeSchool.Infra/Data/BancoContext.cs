0a71745e232d763fe8bd607e372f864a986a2668
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-11 18:28:56 -0300

    feat: Criação contexto

---
+++ FakeSchool.Infra/Data/BancoContext.cs
@@ -0,0 +1,27 @@
+﻿using FakeSchool.Domain.Escola;
+using FakeSchool.Domain.Usuario;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Infra.Data
+{
+	public class BancoContext : IdentityDbContext<Usuario>
+	{
+		public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+		{
+
+		}
+
+
+		public DbSet<Usuario> Usuarios { get; set; }
+		public DbSet<Aluno> Alunos { get; set; }
+		public DbSet<Materia> Materias { get; set; }
+		public DbSet<Nota> Notas{ get; set; }
+		
+	}
+}
