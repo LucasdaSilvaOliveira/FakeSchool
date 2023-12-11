51eb12cf87d37f838eefcd52d1f0d60b267b28df
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-11 18:29:37 -0300

    feat: Mapeamento da tabela de Usuário com identity

---
+++ FakeSchool.Domain/Usuario/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Domain.Usuario
+{
+	public class Usuario : IdentityUser
+	{
+		public Usuario() : base()
+		{
+
+		}
+
+		public virtual string Nome { get; set; }
+
+		public virtual DateTime DataCriacao { get; set; }
+	}
+}
