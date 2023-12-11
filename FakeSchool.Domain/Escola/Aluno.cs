71d6a4636660616598e0e39d3929b1355d96a6cf
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-11 18:29:13 -0300

    feat: Mapeamento das tabelas

---
+++ FakeSchool.Domain/Escola/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Domain.Escola
+{
+	public class Aluno
+	{
+		[Key]
+		public virtual int Id { get; set; }
+
+		public virtual string Nome { get; set; }
+
+		public virtual string Turma { get; set; }
+
+		public virtual string Status { get; set; }
+
+		public virtual int AnoLetivo { get; set; }
+
+		public virtual ICollection<Nota> Notas { get; set; }
+	}
+}
---
+++ FakeSchool.Domain/Escola/Nota.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Domain.Escola
+{
+	public class Nota
+	{
+		[Key]
+		public virtual int Id { get; set; }
+
+		public virtual int AlunoId { get; set; }
+
+		[ForeignKey("AlunoId")]
+		public virtual Aluno Aluno { get; set; }
+
+		public virtual int MateriaId { get; set; }
+
+		[ForeignKey("MateriaId")]
+		public virtual Materia Materia { get; set; }
+
+		public virtual decimal NotaMateria { get; set; }
+	}
+}
