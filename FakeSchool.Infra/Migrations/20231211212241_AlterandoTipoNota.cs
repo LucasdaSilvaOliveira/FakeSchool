02288360cf10e49c9b4c1d58708e2279a975143a
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-11 18:28:35 -0300

    feat: Adicionando tabelas ao banco

---
+++ FakeSchool.Infra/Migrations/20231211212241_AlterandoTipoNota.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FakeSchool.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterandoTipoNota : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NotaMateria",
+                table: "Notas",
+                type: "decimal(3,1)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NotaMateria",
+                table: "Notas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
