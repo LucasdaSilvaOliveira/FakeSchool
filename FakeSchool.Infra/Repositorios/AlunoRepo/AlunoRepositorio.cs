b4663db033d6a3d178c2c763d2b19dd05420bf85
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-12 17:42:51 -0300

    feat: Implementando repositório da entidade Aluno

---
+++ FakeSchool.Infra/Repositorios/AlunoRepo/AlunoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using FakeSchool.Domain.Escola;
+using FakeSchool.Infra.Repositorios.AlunoRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Infra.Repositorios.AlunoRepo
+{
+    public class AlunoRepositorio : IAlunoRepositorio<Aluno>
+    {
+        public Aluno ObterPorId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FakeSchool.Infra/Repositorios/AlunoRepo/IAlunoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeSchool.Domain.Escola;
+
+namespace FakeSchool.Infra.Repositorios.AlunoRepo
+{
+    public interface IAlunoRepositorio<Aluno>
+    {
+        Aluno ObterPorId(int id);
+    }
+}
