52d1b2da61e7377620c69cc5a1b9265603d98de0
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-12 16:54:01 -0300

    add: Adicionando pasta de módulos + início criação tela de alunos

---
+++ FakeSchool.Web/Areas/Aluno/Controllers/AlunoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeSchool.Web.Modules.Aluno.Controllers
+{
+    [Authorize]
+    [Area("Aluno")]
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
