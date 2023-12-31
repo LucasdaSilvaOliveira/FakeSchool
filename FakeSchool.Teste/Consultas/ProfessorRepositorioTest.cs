ea4bf40dfb272f8db756730c3d964473c1fa18b9
Lucas Silva <lucas-silva-98@hotmail.com>
2023-12-31 19:57:33 -0300

    feat: Início implementação de testes de consulta do repositório da entidade Professor

---
+++ FakeSchool.Teste/Consultas/ProfessorRepositorioTest.cs
@@ -0,0 +1,28 @@
+﻿using FakeSchool.Domain.Escola;
+using FakeSchool.Infra.Repositorios.ProfessorRepo;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSchool.Teste.Consultas
+{
+    public class ProfessorRepositorioTest
+    {
+        [Fact]
+        public void TestaObterProfessorPorId()
+        {
+            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
+
+            var professorMoq = new Professor();
+
+            mockProfessorRepositorio.Setup(x => x.ObterPorId(1)).Returns(professorMoq);
+
+            var professor = mockProfessorRepositorio.Object.ObterPorId(1);
+
+            Assert.NotNull(professor);
+        }
+    }
+}
