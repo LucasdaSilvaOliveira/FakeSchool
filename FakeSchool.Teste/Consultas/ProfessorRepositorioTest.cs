0444ba9704a7a2d577fe36ec7285fc1e41c82a87
Lucas Silva <lucas-silva-98@hotmail.com>
2024-01-01 14:24:57 -0300

    feat: Implementação de testes no repositório de professores

--- FakeSchool.Teste/Consultas/ProfessorRepositorioTest.cs
+++ FakeSchool.Teste/Consultas/ProfessorRepositorioTest.cs
@@ -1,4 +1,5 @@
 ﻿using FakeSchool.Domain.Escola;
+using FakeSchool.Infra.Repositorios.CursoRepo;
 using FakeSchool.Infra.Repositorios.ProfessorRepo;
 using Moq;
 using System;
@@ -23,6 +24,55 @@ namespace FakeSchool.Teste.Consultas
             var professor = mockProfessorRepositorio.Object.ObterPorId(1);
 
             Assert.NotNull(professor);
+        }
+
+        [Fact(DisplayName = "Teste que obtém todos os professores")]
+        public void TestaObterTodosProfessores()
+        {
+            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
+
+            var listaProfessoresMoq = new List<Professor>
+            {
+                new Professor { Id = 1,Nome = "Antonio Carlos"},
+                new Professor { Id = 2,Nome = "Ana Ferreira"},
+                new Professor { Id = 3,Nome = "Lya Alves"}
+            };
+
+            mockProfessorRepositorio.Setup(x => x.ObterTodos()).Returns(listaProfessoresMoq);
+
+            var listaProfessores = mockProfessorRepositorio.Object.ObterTodos();
+
+            mockProfessorRepositorio.Verify(x => x.ObterTodos(), Times.Once);
+
+            Assert.NotNull(listaProfessores);
+            Assert.Equal(listaProfessores, listaProfessoresMoq);
+        }
+
+        [Fact(DisplayName = "Teste de remoção de professor")]
+        public void TesteDeletarProfessor()
+        {
+            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
+
+            var listaProfessoresMoq = new List<Professor>
+            {
+                new Professor { Id = 1,Nome = "Antonio Carlos"},
+                new Professor { Id = 2,Nome = "Ana Ferreira"},
+                new Professor { Id = 3,Nome = "Lya Alves"}
+            };
+
+            mockProfessorRepositorio.Setup(x => x.Deletar(It.IsAny<int>())).Callback<int>(id =>
+            {
+                var professor = listaProfessoresMoq.FirstOrDefault(x => x.Id == id);
+
+                if (professor != null)
+                {
+                    listaProfessoresMoq.Remove(professor);
+                }
+            });
+
+            mockProfessorRepositorio.Object.Deletar(1);
+
+            Assert.Equal(2, listaProfessoresMoq.Count);
         }
     }
 }
