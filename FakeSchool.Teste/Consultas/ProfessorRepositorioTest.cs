a4a1acce4520adb9bbe9edee2b607ccaa399907b
Lucas Silva <lucas-silva-98@hotmail.com>
2024-01-02 13:35:34 -0300

    refact: Alteração na classe de teste do módulo de professores

--- FakeSchool.Teste/Consultas/ProfessorRepositorioTest.cs
+++ FakeSchool.Teste/Consultas/ProfessorRepositorioTest.cs
@@ -6,16 +6,21 @@ namespace FakeSchool.Teste.Consultas
 {
     public class ProfessorRepositorioTest
     {
+        Mock<IProfessorRepositorio> _mockProfessorRepositorio;
+        public ProfessorRepositorioTest()
+        {
+            _mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
+        }
+
         [Fact(DisplayName = "Teste que obtém professor por Id")]
         public void TesteObterProfessorPorId()
         {
-            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
 
             var professorMoq = new Professor();
 
-            mockProfessorRepositorio.Setup(x => x.ObterPorId(1)).Returns(professorMoq);
+            _mockProfessorRepositorio.Setup(x => x.ObterPorId(1)).Returns(professorMoq);
 
-            var professor = mockProfessorRepositorio.Object.ObterPorId(1);
+            var professor = _mockProfessorRepositorio.Object.ObterPorId(1);
 
             Assert.NotNull(professor);
         }
@@ -23,8 +28,6 @@ namespace FakeSchool.Teste.Consultas
         [Fact(DisplayName = "Teste que obtém todos os professores")]
         public void TesteObterTodosProfessores()
         {
-            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
-
             var listaProfessoresMoq = new List<Professor>
             {
                 new Professor { Id = 1,Nome = "Antonio Carlos"},
@@ -32,11 +35,11 @@ namespace FakeSchool.Teste.Consultas
                 new Professor { Id = 3,Nome = "Lya Alves"}
             };
 
-            mockProfessorRepositorio.Setup(x => x.ObterTodos()).Returns(listaProfessoresMoq);
+            _mockProfessorRepositorio.Setup(x => x.ObterTodos()).Returns(listaProfessoresMoq);
 
-            var listaProfessores = mockProfessorRepositorio.Object.ObterTodos();
+            var listaProfessores = _mockProfessorRepositorio.Object.ObterTodos();
 
-            mockProfessorRepositorio.Verify(x => x.ObterTodos(), Times.Once);
+            _mockProfessorRepositorio.Verify(x => x.ObterTodos(), Times.Once);
 
             Assert.NotNull(listaProfessores);
             Assert.Equal(listaProfessores, listaProfessoresMoq);
@@ -45,8 +48,6 @@ namespace FakeSchool.Teste.Consultas
         [Fact(DisplayName = "Teste de remoção de professor")]
         public void TesteDeletarProfessor()
         {
-            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
-
             var listaProfessoresMoq = new List<Professor>
             {
                 new Professor { Id = 1,Nome = "Antonio Carlos"},
@@ -54,7 +55,7 @@ namespace FakeSchool.Teste.Consultas
                 new Professor { Id = 3,Nome = "Lya Alves"}
             };
 
-            mockProfessorRepositorio.Setup(x => x.Deletar(It.IsAny<int>())).Callback<int>(id =>
+            _mockProfessorRepositorio.Setup(x => x.Deletar(It.IsAny<int>())).Callback<int>(id =>
             {
                 var professor = listaProfessoresMoq.FirstOrDefault(x => x.Id == id);
 
@@ -64,27 +65,37 @@ namespace FakeSchool.Teste.Consultas
                 }
             });
 
-            mockProfessorRepositorio.Object.Deletar(1);
+            _mockProfessorRepositorio.Object.Deletar(1);
 
             Assert.Equal(2, listaProfessoresMoq.Count);
         }
 
         [Fact(DisplayName = "Teste de atualização de professor")]
-        public void TesteAtualizacaoProfessor()
+        public void TesteAtualizacarProfessor()
         {
-            var mockProfessorRepositorio = new Mock<IProfessorRepositorio>();
-
             var professorMock = new Professor
             {
                 Id = 1,
                 Nome = "Antonio Carlos"
             };
 
-            mockProfessorRepositorio.Setup(x => x.Atualizar(It.IsAny<Professor>()));
+            _mockProfessorRepositorio.Setup(x => x.Atualizar(It.IsAny<Professor>()));
 
-            mockProfessorRepositorio.Object.Atualizar(professorMock);
+            _mockProfessorRepositorio.Object.Atualizar(professorMock);
 
-            mockProfessorRepositorio.Verify(x => x.Atualizar(professorMock), Times.Once);
+            _mockProfessorRepositorio.Verify(x => x.Atualizar(professorMock), Times.Once);
+        }
+
+        [Fact(DisplayName = "Teste de cadastro de professor")]
+        public void TesteCadastroDeProfessor()
+        {
+            var professorMock = new Professor { Id = 1, Nome = "Antonio Carlos" };
+
+            _mockProfessorRepositorio.Setup(x => x.CadastrarProfessor(professorMock));
+
+            _mockProfessorRepositorio.Object.CadastrarProfessor(professorMock);
+
+            _mockProfessorRepositorio.Verify(x => x.CadastrarProfessor(professorMock), Times.Once);
         }
 
     }
